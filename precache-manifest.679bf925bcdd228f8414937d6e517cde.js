595950bbf6fa1bb8e74f480c3e0abf234443f13b
NITESH KHANDUJA <niteshkhanduja19943@gmail.com>
2020-09-23 09:08:24 -0800

    Updates

---
+++ precache-manifest.679bf925bcdd228f8414937d6e517cde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f701600279cef056b5e7c04fa5a9250",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "39e0eaf0bb2b749f72f3",
+    "url": "/project1/static/css/main.faef0c8f.chunk.css"
+  },
+  {
+    "revision": "408ac22e26a9de087bb1",
+    "url": "/project1/static/js/2.aea2c5f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project1/static/js/2.aea2c5f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e0eaf0bb2b749f72f3",
+    "url": "/project1/static/js/main.df96e3a3.chunk.js"
+  },
+  {
+    "revision": "97a7bb867f3e56782e38",
+    "url": "/project1/static/js/runtime-main.653fe6a3.js"
+  }
+]);
